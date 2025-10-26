6c3535666515a9dbe07b8dca73de74c3150fb4b6
corrydautrich <corry.dautrich@icloud.com>
2025-10-26 14:23:35 -0500

    Filled in the script.js portion of the code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 console.log("script.js connected!");
+
+const userAnswers = {};
+
+const questionBlocks = document.querySelectorAll(".question-block");
+
+questionBlocks.forEach((block, index) => {
+    const answerButtons = block.querySelectorAll(".answer-btn");
+    
+    answerButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+
+            answerButtons.forEach((btn) => btn.classList.remove("selected"));
+            
+            button.classList.add("selected");
+
+            const answerValue = button.getAttribute("data-answer");
+            const strokeType = button.getAttribute("data-stroke");
+
+            userAnswers[`question-${index + 1}`] = {
+                answer: answerValue,
+                stroke: strokeType,
+            };
+            console.log(userAnswers);
+        });
+    });
+});
